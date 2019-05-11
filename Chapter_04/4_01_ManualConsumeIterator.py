c72ddff40c174d711e763c8178d46612c994a87b
winterpading <@>
2019-05-11 21:06:48 +0100

    Worked example 4_01 - Manually consuming an Iterator

--- Chapter_04/4_01_ManualConsumeIterator.py
+++ Chapter_04/4_01_ManualConsumeIterator.py
@@ -1,7 +1,25 @@
-with open('./4_01_data.txt') as fh:
-    try:
+def method_one(filepath):
+    with open(filepath) as fh:
+        try:
+            while True:
+                line = next(fh)
+                print(line, end='')
+        except StopIteration:
+            pass
+
+
+def method_two(filepath):
+    with open(filepath) as fh:
         while True:
-            line = next(fh)
+            line = next(fh, None)
+            if line is None:
+                break
             print(line, end='')
-    except StopIteration:
-        pass
+
+
+file = './4_01_data.txt'
+print('Method 1')
+method_one(file)
+print('\n')
+print('Method 2')
+method_two(file)
