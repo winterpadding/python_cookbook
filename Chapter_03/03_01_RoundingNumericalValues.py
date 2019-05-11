f0f9689a8b19cb62f588f3abf290d07a96fb31f6
winterpading <@>
2019-05-11 15:01:19 +0100

    Worked example 3_01

---
+++ Chapter_03/03_01_RoundingNumericalValues.py
@@ -0,0 +1,15 @@
+print(round(1.23, 1))
+print(round(1.27, 1))
+print(round(-1.27, 1))
+print(round(1.25361, 3))
+
+# Note these will both round to 2.
+# When a value is exactly halfway between two choices -> round to the nearest EVEN number
+print(round(1.5, 0))
+print(round(2.5, 0))
+
+a = 1627731
+print(a)
+print(round(a, -1))
+print(round(a, -2))
+print(round(a, -3))
